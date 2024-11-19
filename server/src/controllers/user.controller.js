3227cc25966f005de8da947bd3390217bd3d1707
ojas-17 <ojasbalsaraf@gmail.com>
2024-11-19 17:10:12 +0530

    log req cookies

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -116,7 +116,7 @@ const loginUser = asyncHandler(async (req, res) => {
     const options = {
         httpOnly: true,
         secure: true,
-        sameSite: 'Strict',
+        sameSite: 'None',
     }
 
     // console.log({accessToken, refreshToken});
